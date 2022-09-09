79259cbdf3e6407cc9189b75c767b68c7d8ed6af
Nataliya Pogrebniak <pogrebnyak.nataliya@gmail.com>
2022-09-09 21:25:36 +0300

    add footer modal

---
+++ src/js/footer-team.js
@@ -0,0 +1,63 @@
+const team = [
+  { group: 'project-group-2' },
+  { teamName: 'SVARDEN' },
+  { teamNameDecoding: 'alternative code command of real development' },
+  {
+    teamLead: {
+      name: 'Artem Bahmet',
+      link: 'https://github.com/bagetmasta',
+    },
+  },
+  {
+    scrumMaster: {
+      name: 'Nataliia Fedorova',
+      link: 'https://github.com/NatashaFedorova',
+    },
+  },
+  {
+    developers: [
+      {
+        name: 'Shpak Rodion',
+        link: 'https://github.com/RodionShpak',
+      },
+      {
+        name: 'Nataliia Pogrebniak',
+        link: 'https://github.com/PNataliya',
+      },
+      {
+        name: '',
+        link: '',
+      },
+      {
+        name: '',
+        link: '',
+      },
+      {
+        name: '',
+        link: '',
+      },
+      {
+        name: '',
+        link: '',
+      },
+      {
+        name: '',
+        link: '',
+      },
+      {
+        name: '',
+        link: '',
+      },
+      {
+        name: '',
+        link: '',
+      },
+      {
+        name: '',
+        link: '',
+      },
+    ],
+  },
+];
+
+export { team };
