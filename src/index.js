8a9cae8d2d243d58633952ff7dda67487fba56f4
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2022-09-10 16:41:15 +0300

    bag-fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import './js/onCardLinkClick';
-import './js/onModalCloseBtn';
 import './js/popualar-films';
 import './js/movie-search';
+
+import onCardLinkClick from './js/onCardLinkClick';
+import onModalCloseBtn from './js/onModalCloseBtn';
+import scrollToTop from './js/scrollToTop';
