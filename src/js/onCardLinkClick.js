b0ce22cb972345a728a921bb3ce87552cf5e5451
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2022-09-10 14:15:30 +0300

    close_modal

--- src/js/onCardLinkClick.js
+++ src/js/onCardLinkClick.js
@@ -1,3 +1,5 @@
+import { onEscapeClose } from './onModalCloseBtn';
+
 const cardLinks = document.querySelectorAll('.card-list__link');
 const filmModal = document.querySelector('.backdrop');
 
@@ -5,7 +7,11 @@ for (let cardLink of cardLinks) {
   cardLink.addEventListener('click', onCardLinkClick);
 }
 
- export default function onCardLinkClick(e) {
-    e.preventDefault();
-    filmModal.classList.remove('is-hidden');
-};
+export default function onCardLinkClick(e) {
+  e.preventDefault();
+  filmModal.classList.remove('is-hidden');
+  // ===
+  if (!filmModal.classList.contains('is-hidden')) {
+    onEscapeClose();
+  }
+}
