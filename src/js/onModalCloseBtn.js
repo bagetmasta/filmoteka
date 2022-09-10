fba7347c6148ec88bb6749ebc465d361b7e22567
Nikita Rohovoi <NRohovoi@gmail.com>
2022-09-10 08:55:24 +0300

    sorry

--- src/js/onModalCloseBtn.js
+++ src/js/onModalCloseBtn.js
@@ -2,6 +2,7 @@ const closeModalBtn = document.querySelector('.modal--icon');
 const filmModal = document.querySelector('.backdrop');
 
 closeModalBtn.addEventListener('click', onModalCloseBtn)
+
 export default function onModalCloseBtn(e) {
-        filmModal.classList.add('is-hidden');
+    filmModal.classList.add('is-hidden');
 }
