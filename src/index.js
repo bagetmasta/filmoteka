bb1d48a0d7dbeaa6acd7f54f37ee12a0c9838669
Nikita Rohovoi <NRohovoi@gmail.com>
2022-09-10 20:53:03 +0300

    ...

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './js/popualar-films';
 import './js/movie-search';
+import './js/modal-footer';
 
 // import onCardLinkClick from './js/onCardLinkClick';
 import onModalCloseBtn from './js/onModalCloseBtn';
-import scrollToTop from './js/scrollToTop';
+// import scrollToTop from './js/scrollToTop';
