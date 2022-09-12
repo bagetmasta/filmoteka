67c217b260d232e89610d784128573bfe316b214
Veronika Savluk <s_veronica@ukr.net>
2022-09-12 17:07:55 +0300

    render watched page

---
+++ src/js/components/rendergenremovebyname.js
@@ -0,0 +1,13 @@
+import { refs } from "./constants-library";
+
+export default function renderGenreMovieByName(film) {
+    const genreNames = [];
+    film.genres.map(genre => {
+    return genreNames.push(genre.name);
+    });
+    if (genreNames.length > 2) {
+        return refs.movieGenre = `${genreNames.slice(0, 2).join(", ")}, others`
+    } else {
+        return refs.movieGenre = genreNames.join(", ");
+    }
+};
---
+++ src/js/components/renderwatchedpage.js
@@ -0,0 +1,35 @@
+import { refs } from './constants-library';
+import renderGenreMovieByName from './rendergenremovebyname';
+
+let parsedObjectWathedfilmes = JSON.parse(localStorage.getItem("watched"));
+
+refs.watchedBtn.addEventListener("click", onWatchedBtn);
+function onWatchedBtn(e) {
+    e.preventDefault();
+
+    if (!parsedObjectWathedfilmes) {
+        return;
+    } else {
+
+        if (!refs.imgStub.classList.contains("is-hidden")){
+            refs.imgStub.classList.add("is-hidden");
+        };
+
+        let libraryWatchedPost = parsedObjectWathedfilmes.map(film => {
+            renderGenreMovieByName(film);
+            return ` <li class="card-list__item">
+                        <a href="" class="card-list__link">
+                            <picture class="card-list_picture">
+                            <img src="https://image.tmdb.org/t/p/original${film.poster_path}" alt="${film.original_title}">
+                            </picture>
+                            <h2 class="card-list__title"><span class="card-list__movie-name">${film.original_title}</span>
+                            ${refs.movieGenre} | ${new Date(film.release_date).getFullYear()}<span class="card-list__ratimg">${film.vote_average.toFixed(1)}</span>
+                            </h2>
+                        </a>
+                    </li>`;
+    })
+            .join('');
+        refs.library.innerHTML = "";
+    refs.library.insertAdjacentHTML("beforeend", `${libraryWatchedPost}`);
+}
+}
