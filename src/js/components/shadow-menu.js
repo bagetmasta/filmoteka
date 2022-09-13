ac19312ad019d123038500833bd9fdd95f40af95
Stas Korobchenko <stanislav.govoruhin@gmail.com>
2022-09-13 23:40:01 +0300

    scroll up

--- src/js/components/shadow-menu.js
+++ src/js/components/shadow-menu.js
@@ -1,9 +1,17 @@
 const shadow = document.querySelector('.shadow-background');
 document.body.style.overflow = 'hidden';
 
+function backToTop() {
+    if (window.pageYOffset > 0) {
+      window.scrollBy(0, -80);
+      setTimeout(backToTop, 0);
+    }
+}
+  
 function shadowOf() {
   shadow.classList.add('set-timeout');
   document.body.style.overflow = 'visible';
 }
 
+backToTop() 
 setTimeout(shadowOf, 4000);
