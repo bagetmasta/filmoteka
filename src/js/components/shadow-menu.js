c42f33dc7a8860de3f71e0accf9b1432c4c82c75
Artem Bahmet <baget.masta@gmail.com>
2022-09-13 14:04:08 +0300

    Update shadow-menu.js

--- src/js/components/shadow-menu.js
+++ src/js/components/shadow-menu.js
@@ -2,9 +2,8 @@ const shadow = document.querySelector('.shadow-background');
 document.body.style.overflow = 'hidden';
 
 function shadowOf() {
-    console.log(shadow)
-    shadow.classList.add('set-timeout');
-    document.body.style.overflow = 'visible';
-};
+  shadow.classList.add('set-timeout');
+  document.body.style.overflow = 'visible';
+}
 
 setTimeout(shadowOf, 4000);
