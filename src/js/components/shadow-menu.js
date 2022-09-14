aecc996c27b97e930b936bcef178f00bae9e4a61
Artem Bahmet <baget.masta@gmail.com>
2022-09-14 21:40:58 +0300

    save

--- src/js/components/shadow-menu.js
+++ src/js/components/shadow-menu.js
@@ -1,16 +1,10 @@
 const shadow = document.querySelector('.shadow-background');
 document.body.style.overflow = 'hidden';
 
-function backToTop() {
-  if (window.pageYOffset > 0) {
-    window.scrollBy(0, -80);
-  }
-}
-
 function shadowOf() {
   shadow.classList.add('set-timeout');
   document.body.style.overflow = 'visible';
 }
 
-backToTop();
+// backToTop();
 setTimeout(shadowOf, 4000);
