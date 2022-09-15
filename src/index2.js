2d84e51ca423d5218190fbb8d1538ef29634cef9
Artem Bahmet <baget.masta@gmail.com>
2022-09-15 16:52:36 +0300

    Update index2.js

--- src/index2.js
+++ src/index2.js
@@ -3,4 +3,3 @@ import './js/components/renderwatchedpage';
 import './js/queue-button';
 import './js/components/switcher';
 import './js/components/btn-load-more';
-import './js/components/on-modal-close-btn';
