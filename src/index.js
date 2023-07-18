79ee5a1a015879141cbb8e3855156e44e4e9b0c1
Artem Bahmet <baget.masta@gmail.com>
2023-07-18 11:17:09 +0300

    change parcel configuration

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
-// import './js/popular-films';
-// import './js/movie-search';
 import './js/main';
-// import './js/components/onCardLinkClick';
 import './js/components/modal-footer';
 import './js/components/to-animated-card';
 import './js/components/switcher';
-
-// Ниже приянятый пул от Андрея
-// import './js/popular-films';
-// import './js/movie-search';
-// import './js/modal-footer';
-// import './js/preloaders';
-// import './js/localstorage-btn-watched';
-
-// import onCardLinkClick from './js/onCardLinkClick';
-// import onModalCloseBtn from './js/components/onModalCloseBtn';
 import scrollToTop from './js/components/scroll-to-top';
 import shadowMenu from './js/components/shadow-menu';
